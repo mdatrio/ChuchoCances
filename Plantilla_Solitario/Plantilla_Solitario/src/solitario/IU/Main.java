1be002f71f0dc138aa3c9466452abe310a62c7a0
manupe21 <46379066+manupe21@users.noreply.github.com>
2020-04-13 18:48:04 +0200

    Hola

--- Plantilla_Solitario/Plantilla_Solitario/src/solitario/IU/Main.java
+++ Plantilla_Solitario/Plantilla_Solitario/src/solitario/IU/Main.java
@@ -4,6 +4,7 @@ package solitario.IU;
 import java.util.ArrayList;
 import solitario.Core.Baraja;
 import static solitario.IU.Solitario.inicioPartida;
+import solitario.Core.*;
 
 /**
  *
@@ -14,6 +15,11 @@ public class Main {
  public static void main(String[] args) {              
 
 //inicioPartida();
+ 
+        Baraja b=new Baraja();
+        b.crearCartas();
+        b.barajarCartas();
+        System.out.println(b.toString());
 
 }
 
